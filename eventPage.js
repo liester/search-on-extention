baeb66b0ae9b01e6455d8026ce0eb0f42aea6dad
Levi Liester <levi.liester@gmail.com>
2019-09-04 09:05:04 -0500

    All the stuffs, published

--- eventPage.js
+++ eventPage.js
@@ -6,7 +6,7 @@ const trackEvent = (category, action, label) => {
         // API Version.
         v: '1',
         // Tracking ID / Property ID.
-        tid: "UA-146926077-1",
+        tid: "UA-291821-1",
         // Anonymous Client Identifier. Ideally, this should be a UUID that
         // is associated with particular user, device, or browser instance.
         cid: '1',
@@ -15,9 +15,7 @@ const trackEvent = (category, action, label) => {
         // Event category.
         ec: category,
         // Event action.
-        ea: action,
-        // Event label.
-        el: label,
+        ea: action
     };
 
     return $.post('http://www.google-analytics.com/collect', data);
@@ -29,11 +27,14 @@ const contextMenuItem = {
     "contexts": ["selection"]
 };
 
+const medium = "search-chrome-extension";
 chrome.contextMenus.create(contextMenuItem);
 
 chrome.contextMenus.onClicked.addListener(function (clickData) {
     console.log(clickData.selectionText);
-    trackEvent("search-on-proxibid", clickData.selectionText||"unknown",'search');
-    const searchURL = `https://www.proxibid.com/asp/SearchAdvanced_i.asp?searchTerm=${clickData.selectionText}&category=all%20categories&timestamp=694#searchid=0&type=auctionCompany&search=${clickData.selectionText}&sort=relevance&view=gallery&length=25&refine=&category=all%20categories&start=1`
+
+    trackEvent(medium, clickData.selectionText||"unknown");
+
+    const searchURL = `https://www.proxibid.com/asp/SearchAdvanced_i.asp?searchTerm=${clickData.selectionText}&utm_source=${clickData.pageUrl}&utm_medium=${medium}&utm_campaign=${medium}`
     chrome.tabs.create({url: searchURL});
 })
